af08b96e59b5504ccc9140165355319ad4e7036c
Dibbopro <94396845+Dibbopro@users.noreply.github.com>
2025-05-11 12:51:10 +0600

    Update particles-config.js

--- particles-config.js
+++ particles-config.js
@@ -2,27 +2,22 @@ particlesJS("particles-js", {
   particles: {
     number: {
       value: 40,
-      density: { enable: true, value_area: 1000 }
+      density: { enable: true, value_area: 900 }
     },
     color: {
-      value: ["#00ff00", "#55ff55", "#88ff88"]  // Green Minecraft-style particles
+      value: ["#00ff00", "#55ff55", "#88ff88"]  // MC green
     },
     shape: {
-      type: "square", // makes them pixel-like
+      type: "square",
       stroke: { width: 0, color: "#000" }
     },
     opacity: {
       value: 0.8,
       random: true,
-      anim: {
-        enable: true,
-        speed: 0.4,
-        opacity_min: 0.4,
-        sync: false
-      }
+      anim: { enable: true, speed: 0.2, opacity_min: 0.4, sync: false }
     },
     size: {
-      value: 3,
+      value: 4,
       random: true
     },
     line_linked: {
@@ -30,14 +25,14 @@ particlesJS("particles-js", {
     },
     move: {
       enable: true,
-      speed: 0.6,
+      speed: 0.5,
       direction: "top",
       random: true,
-      straight: false,
       out_mode: "out"
     }
   },
   interactivity: {
+    detect_on: "canvas",
     events: {
       onhover: { enable: false },
       onclick: { enable: false },
